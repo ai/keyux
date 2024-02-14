9ce345401c7d2fdfef67cb049b8cc8aa9eeaa1c0
Andrey Sitnik <andrey@sitnik.ru>
2024-02-14 21:38:19 +0100

    Improve tests

--- test/press.test.ts
+++ test/press.test.ts
@@ -25,6 +25,30 @@ test('adds pressed state', () => {
   equal(button.classList.contains('is-hover'), false)
 })
 
+test('ignores mouse click', () => {
+  let window = new JSDOM().window
+  startKeyUX(window, [pressKeyUX('is-pressed')])
+
+  window.document.body.innerHTML = '<button></button>'
+  let button = window.document.querySelector('button')!
+  equal(button.classList.contains('is-pressed'), false)
+
+  button.dispatchEvent(
+    new window.MouseEvent('click', { bubbles: true, clientX: 0, clientY: 10 })
+  )
+  equal(button.classList.contains('is-pressed'), false)
+
+  button.dispatchEvent(
+    new window.MouseEvent('click', { bubbles: true, clientX: 0, clientY: 0 })
+  )
+  equal(button.classList.contains('is-pressed'), true)
+
+  window.dispatchEvent(
+    new window.KeyboardEvent('keyup', { bubbles: true, key: 'Enter' })
+  )
+  equal(button.classList.contains('is-pressed'), false)
+})
+
 test('stops event tracking', () => {
   let window = new JSDOM().window
   let stop = startKeyUX(window, [
