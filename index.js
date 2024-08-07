e622ac5e5b0a0651d65d7fde346774f4c62cf3fa
Andrey <shevchenko.andrey.uzurpastor@gmail.com>
2024-08-07 20:16:55 +0300

    jump to first element on esc -> implement

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ export * from './hotkey.js'
 export * from './jump.js'
 export * from './overrides.js'
 export * from './press.js'
+export * from './escape.js'
 
 export function startKeyUX(window, plugins) {
   let unbinds = plugins.map(plugin => plugin(window))
